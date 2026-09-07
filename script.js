db21d6a3af794f0c74281ac265ba2d2b2e535b9b
Tanu Rana <tanu16854@gmail.com>
2026-09-07 12:43:20 +0530

    Implement add task feature

---
+++ script.js
@@ -0,0 +1,14 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+addBtn.addEventListener("click", addTask);
+function addTask() {
+const taskText = taskInput.value.trim();
+if (taskText === "") {
+    return;
+}
+const li = document.createElement("li");
+li.textContent = taskText;
+taskList.appendChild(li);
+taskInput.value = "";
+}
