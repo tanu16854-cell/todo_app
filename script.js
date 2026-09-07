cac948d8177c9957a17f90d1e0325f534b3f71a9
Tanu Rana <tanu16854@gmail.com>
2026-09-07 13:30:09 +0530

    Improve task layout and interactions

--- script.js
+++ script.js
@@ -2,26 +2,35 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 addBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+    addTask();
+}
+});
 function addTask() {
 const taskText = taskInput.value.trim();
 if (taskText === "") {
     return;
 }
 const li = document.createElement("li");
+li.classList.add("task");
+const taskSpan = document.createElement("span");
 taskSpan.textContent = taskText;
 
-taskList.appendChild(li);
+
 taskInput.value = "";
 
 const deleteBtn = document.createElement("button");
+deleteBtn.classList.add("deleteBtn");
 deleteBtn.textContent = "Delete";
 li.appendChild(deleteBtn);
 deleteBtn.addEventListener("click", function() {
     li.remove();
 });
-const taskSpan = document.createElement("span");
+
 li.appendChild(taskSpan);
 taskSpan.addEventListener("click", function() {
     taskSpan.classList.toggle("completed");
 });
+taskList.appendChild(li);
 }
